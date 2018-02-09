85db840441659c105c96180cb36f2b1db51f3c53
heath smith <heathsmith73@gmail.com>
2018-02-08 16:00:02 -0800

    updated my post signup

--- __test__/route/user/user-signup.test.js
+++ __test__/route/user/user-signup.test.js
@@ -37,7 +37,7 @@ describe('POST /api/v1/signup', function() {
   
   describe('Invalid req/res', () => {
     it('should return a status 404 on bad path', () => {
-      return superagent.post(':4000/api/v1/doesNotExist')
+      return superagent.post(':4000/api/v1/')
         .send(mocks.user.createOne())
         .catch(err => {
           expect(err.status).toBe(404);
